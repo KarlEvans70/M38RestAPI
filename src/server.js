29482977c1102c9d2911f117198fbc94015eafcd
Karl Evans <karlevans70@hotmail.com>
2022-07-20 17:29:57 +0100

    changes to update and delete

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require("./db/connection"); //instantly run database connection
 const express = require("express"); // pull in all of express module
 const userRouter = require("./user/routes"); // bring in all endpoints connected to userRouter
 const app = express(); // create webserver constant to manipulate
-const port = process.env.PORT || 5001; // store either supplied port or 5001
+const port = process.env.PORT || 6000; // store either supplied port or 5001
 
 app.use(express.json()); // parses all requests as if they are JSON, sends all responses as JSON
 
